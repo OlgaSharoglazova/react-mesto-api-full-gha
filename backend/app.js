10fd6616c49c5ce04bb23064098e5a80ac7a83d3
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-16 16:00:49 +0800

    логи

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const { NOT_FOUND } = require('./utils/constants');
 const errorsCode = require('./middlewares/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 
+app.use(requestLogger);
 app.use(routes);
 
 app.use('*', (_req, res) => {
@@ -22,6 +24,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsCode);
 
