6940bd99e21b119ea7f84728dd083b327eae34ab
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-18 00:43:07 +0800

    пр 15 без фронта

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ isLoggedIn, element: Component, ...props  }) => {
-  return (
-    isLoggedIn ? <Component {...props} /> : <Navigate to="/signup" replace/>
-)}
-
-export default ProtectedRoute; 
