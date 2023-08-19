c0a42981a33c787371e14155e4f6380622750867
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-19 17:44:07 +0800

    контроллер логин после ревью

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,7 +6,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const ConflictError = require('../errors/conflictError');
 const NotFound = require('../errors/notFound');
-const Unauthorized = require('../errors/unauthorized');
+// const Unauthorized = require('../errors/unauthorized');
 const BadRequest = require('../errors/badRequest');
 
 module.exports.getUsers = (_req, res, next) => {
@@ -90,9 +90,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token, user });
     })
-    .catch(() => {
-      next(new Unauthorized('Ошибка авторизации'));
-    });
+    .catch(next);
 };
 
 module.exports.updateUser = (req, res, next) => {
