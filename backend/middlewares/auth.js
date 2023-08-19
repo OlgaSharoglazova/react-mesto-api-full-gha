1ecd765897be1d3bcddd50850ddd19b80b695b0a
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-19 19:13:08 +0800

    авторизация после ревью

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/unauthorized');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -6,9 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new Unauthorized('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -17,9 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new Unauthorized('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
