d3d71ca48f8cb4d3d497b57e121db8e53aff187f
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-20 13:46:25 +0800

    контроллер логин после 2 ревью

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -81,7 +81,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.send({ token, user });
+      res.send({ token });
     })
     .catch(next);
 };
